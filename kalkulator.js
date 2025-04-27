c4e60584fa8b44cef6b7493651b71aea31068640
muhamadfazri15 <sendalsunda15@gmail.com>
2025-04-27 21:54:08 +0700

    Update kalkulator.js

--- kalkulator.js
+++ kalkulator.js
@@ -44,7 +44,7 @@ function calculate(operator) {
                     showError('Tidak bisa membagi dengan nol.');
                     return;
                 }
-                result1 = (angka1 / angka2).toFixed(3);
+                result1 = (angka1 / angka2).toFixed(9);
             }
             else if (operator === '%') result1 = (angka1 / 100) * angka2;
             displayResult = `${result1}`;
@@ -58,8 +58,8 @@ function calculate(operator) {
                 showError('Tidak bisa menghitung akar dari bilangan negatif.');
                 return;
             }
-            result1 = angka1 !== null ? Math.sqrt(angka1)toFixed(8) : '-';
-            result2 = angka2 !== null ? Math.sqrt(angka2)toFixed(8) : '-';
+            result1 = angka1 !== null ? Math.sqrt(angka1).toFixed(9) : '-';
+            result2 = angka2 !== null ? Math.sqrt(angka2).toFixed(9) : '-';
             displayResult = `√${angka1 ?? ''}=${result1} | √${angka2 ?? ''}=${result2}`;
             break;
         case 'sin':
@@ -67,8 +67,8 @@ function calculate(operator) {
                 showError('Minimal satu angka harus diisi untuk operasi sin!');
                 return;
             }
-            result1 = angka1 !== null ? Math.sin(toRadians(angka1)).toFixed(9) : '-';
-            result2 = angka2 !== null ? Math.sin(toRadians(angka2)).toFixed(9) : '-';
+            result1 = angka1 !== null ? Math.sin(toRadians(angka1)).toFixed(3) : '-';
+            result2 = angka2 !== null ? Math.sin(toRadians(angka2)).toFixed(3) : '-';
             displayResult = `sin(${angka1 ?? ''})=${result1} | sin(${angka2 ?? ''})=${result2}`;
             break;
         case 'cos':
@@ -76,8 +76,8 @@ function calculate(operator) {
                 showError('Minimal satu angka harus diisi untuk operasi cos!');
                 return;
             }
-            result1 = angka1 !== null ? Math.cos(toRadians(angka1)).toFixed(9) : '-';
-            result2 = angka2 !== null ? Math.cos(toRadians(angka2)).toFixed(9) : '-';
+            result1 = angka1 !== null ? Math.cos(toRadians(angka1)).toFixed(3) : '-';
+            result2 = angka2 !== null ? Math.cos(toRadians(angka2)).toFixed(3) : '-';
             displayResult = `cos(${angka1 ?? ''})=${result1} | cos(${angka2 ?? ''})=${result2}`;
             break;
         case 'tan':
@@ -85,8 +85,8 @@ function calculate(operator) {
                 showError('Minimal satu angka harus diisi untuk operasi tan!');
                 return;
             }
-            result1 = angka1 !== null ? Math.tan(toRadians(angka1)).toFixed(9) : '-';
-            result2 = angka2 !== null ? Math.tan(toRadians(angka2)).toFixed(9) : '-';
+            result1 = angka1 !== null ? Math.tan(toRadians(angka1)).toFixed(3) : '-';
+            result2 = angka2 !== null ? Math.tan(toRadians(angka2)).toFixed(3) : '-';
             displayResult = `tan(${angka1 ?? ''})=${result1} | tan(${angka2 ?? ''})=${result2}`;
             break;
         default:
